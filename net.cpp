1b3f2dcf1546a99bf468ce412ddb89ebe382542c
Maxeg13 <chibisaincrada@gmail.com>
2017-09-26 15:10:47 +0400

    hello

--- net.cpp
+++ net.cpp
@@ -180,7 +180,7 @@ void CNet::kohonSettings()
     for(int i=2;i<10;i++)
     {
         neuron[i].weight[i+8]=6.75;
-//        neuron[i].setRandomWeight(i+8,1);
+
         neuron[i].STDP_set[i+8]=0;
         neuron[i+8].setRandomWeight(0,1);
     }
